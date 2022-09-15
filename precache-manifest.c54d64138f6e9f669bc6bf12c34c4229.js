e520459d1e64517e0edaf5bc7d32fc45efbb7fcb
George Erokhin <Georgii070684@gmail.com>
2022-09-15 14:10:47 +0300

    Updates

---
+++ precache-manifest.c54d64138f6e9f669bc6bf12c34c4229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cea94dbaddac9770b747fdc48232b2",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "5a909e9d526f85f774e1",
+    "url": "/TodoList/static/css/main.2fd5699f.chunk.css"
+  },
+  {
+    "revision": "3822e44219c14f27e5be",
+    "url": "/TodoList/static/js/2.7ec734d2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.7ec734d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a909e9d526f85f774e1",
+    "url": "/TodoList/static/js/main.a8fa14f4.chunk.js"
+  },
+  {
+    "revision": "d35d623f929282414f0c",
+    "url": "/TodoList/static/js/runtime-main.0ff14eb0.js"
+  }
+]);
