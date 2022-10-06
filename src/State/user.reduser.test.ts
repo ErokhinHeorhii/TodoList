144eb382676fc4f6c2405070ae80075f0cba0ed7
George Erokhin <Georgii070684@gmail.com>
2022-10-06 13:50:57 +0300

    feat:  add solution of 7 lesson(without new branch)

---
+++ src/State/user.reduser.test.ts
@@ -0,0 +1,31 @@
+import { userReducer } from "./user-reduser";
+
+test.skip("user reducer should increment only age", () => {
+  const startState = { age: 20, childrenCount: 2, name: "Dimych" };
+
+  const endState = userReducer(startState, { type: "INCREMENT-AGE" });
+
+  expect(endState.age).toBe(21);
+  expect(endState.childrenCount).toBe(2);
+});
+
+test.skip("user reducer should increment only childrenCount", () => {
+  const startState = { age: 20, childrenCount: 2, name: "Dimych" };
+
+  const endState = userReducer(startState, {
+    type: "INCREMENT-CHILDREN-COUNT",
+  });
+
+  expect(endState.childrenCount).toBe(3);
+  expect(endState.age).toBe(20);
+});
+
+test.skip("user reduser should change name of user", () => {
+  const startState = { name: "Dimych", age: 20, childrenCount: 2 };
+  const newName = "Victor";
+  const endState = userReducer(startState, {
+    type: "CHANGE-NAME",
+    newName: newName,
+  });
+  expect (endState.name).toBe(newName)
+});
