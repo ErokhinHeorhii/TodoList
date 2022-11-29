b60739ff3b91f85954fea504c9ebf2e1f54fbbc6
George Erokhin <Georgii070684@gmail.com>
2022-11-29 11:56:12 +0300

    feat: add useCallback and react.Memo for memoiz.

---
+++ src/TaskComponent.tsx
@@ -0,0 +1,39 @@
+import React, {ChangeEvent, useCallback} from "react"
+import {Checkbox, IconButton} from "@mui/material";
+import EditableSpan from "./Components/EditableSpan";
+import {Delete} from "@mui/icons-material";
+import {TaskType} from "./TodoList";
+
+ type TaskComponentType={
+    itemTask:TaskType
+     todolistID:string
+     removeTask:(taskId: string, todolistId: string) => void
+     changeTaskStatus: ( todolistId: string, id: string, isDone: boolean) => void
+     changeTaskTitle: (taskId: string, newTitle: string, todolistId: string) => void
+ }
+
+export const Task= React.memo((props:TaskComponentType)=>{
+
+    const changeTaskTitle = useCallback((title: string) => {
+        props.changeTaskTitle(props.todolistID, props.itemTask.id, title)
+    },[props.changeTaskTitle,props.todolistID,props.itemTask.id])
+
+    const changeStatusCheckbox = (e: ChangeEvent<HTMLInputElement>) => {
+        props.changeTaskStatus(props.todolistID, props.itemTask.id, e.currentTarget.checked)
+    }
+
+    const removeTaskHandler =()=>{
+        props.removeTask(props.todolistID, props.itemTask.id)
+    }
+
+    return (
+        <li key={props.itemTask.id} className={props.itemTask.isDone ? 'isDone' : ""}>
+            <Checkbox color='primary' onChange={changeStatusCheckbox} checked={props.itemTask.isDone}/>
+            <EditableSpan title={props.itemTask.title} changeTitle={changeTaskTitle}/>
+            <IconButton aria-label="delete" size="small"
+                        onClick={removeTaskHandler}>
+                <Delete fontSize="small"/>
+            </IconButton>
+        </li>
+    )
+})
